3fce8373958dfcf4346e9418a0ebcae29797f1af
Zawadi Done <21525407+Zawadidone@users.noreply.github.com>
2025-09-04 15:02:26 +0200

    Improve mapping and settings Elasticsearch template
    Fixes the JSON object `mapping`

--- timesketch-ec-index-template.tf
+++ timesketch-ec-index-template.tf
@@ -1,62 +1,79 @@
-locals {
-  # Elastic field types https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-types.html
-  mappings = jsonencode({
-    # Timesketch mandatory fields
-    properties = {
-      datetime = {
-        type = "date"
-      }
-      timestamp_desc = {
-        type = "keyword"
-      }
-      data_type = {
-        type = "keyword"
-      }
-      timesketch_label = {
-        type = "nested"
-        properties = {
-          name = {
-            type = "text"
-            fields = {
-              keyword = {
-                type = "keyword"
+resource "elasticstack_elasticsearch_component_template" "timesketch-timelines" {
+  name = "timesketch-timelines"
+
+  template {
+    mappings = jsonencode({
+      # Timesketch mandatory fields
+      properties = {
+        datetime = {
+          type = "date"
+        }
+        timestamp_desc = {
+          type = "keyword"
+        }
+        data_type = {
+          type = "keyword"
+        }
+        timesketch_label = {
+          type = "nested"
+          properties = {
+            name = {
+              type = "text"
+              fields = {
+                keyword = {
+                  type = "keyword"
+                }
               }
             }
-          }
-          sketch_id = {
-            type = "long"
-          }
-          user_id = {
-            type = "long"
+            sketch_id = {
+              type = "long"
+            }
+            user_id = {
+              type = "long"
+            }
           }
         }
+        tag = {
+          type = "keyword"
+        }
+        __ts_timeline_id = {
+          type = "long"
+        }
       }
-      tag = {
-        type = "keyword"
-      }
-      __ts_timeline_id = {
-        type = "long"
-      }
-    }
 
-    # The other fields are mapped to as object or wildcard.
-    dynamic_templates = [
-      {
-        object = {
-          match_mapping_type = "object"
-          mapping = {
-            type = "object"
+      # The other fields are mapped as object or wildcard.
+      dynamic_templates = [
+        {
+          object = {
+            match_mapping_type = "object"
+            mapping = {
+              type = "object"
+            }
+          }
+        },
+        {
+          wildcard = {
+            match_mapping_type = "*"
+            mapping = {
+              type = "wildcard"
+            }
           }
         }
-      },
-      {
-        wildcard = {
-          match_mapping_type = "*"
-          mapping = {
-            type = "wildcard"
-          }
+      ]
+    })
+
+    settings = jsonencode({
+      mapping = {
+        total_fields = {
+          limit = 20000
         }
       }
-    ]
-  })
+    })
+  }
+
+  elasticsearch_connection {
+    endpoints = [<ENDPOINT>]
+    username  = <USERNAME>
+    password  = <PASSWORD>
+  }
 }
